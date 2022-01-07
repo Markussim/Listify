05a9b0c31285dddf631b1aa8f2fbdb173264f1d4
Markus Simonsen <33492046+Markussim@users.noreply.github.com>
2022-01-07 15:15:08 +0100

    Added pwa support

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,10 +3,11 @@ import App from './App.vue'
 import router from './router'
 import VueSocketIO from 'vue-3-socket.io'
 import io from 'socket.io-client'
+import './registerServiceWorker'
 
 const socketio = new VueSocketIO({
     debug: false,
-    connection: io('http://192.168.10.118:3000'),
+    connection: io('http://localhost:3000'),
   });
 
 createApp(App).use(router).use(socketio).mount('#app')
