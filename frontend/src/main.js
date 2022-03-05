7f4017310af041f28ec6114e0092487d75545701
Markus Simonsen <33492046+Markussim@users.noreply.github.com>
2022-03-05 14:32:22 +0100

    firebase init

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,18 @@ import App from "./App.vue";
 import router from "./router";
 import VueSocketIO from "vue-3-socket.io";
 import io from "socket.io-client";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBSk6Goi--7CeGNJ5Vx-Vcv_Ncn7PMU7p0",
+  authDomain: "listify-df84d.firebaseapp.com",
+  projectId: "listify-df84d",
+  storageBucket: "listify-df84d.appspot.com",
+  messagingSenderId: "137405468391",
+  appId: "1:137405468391:web:3dd23342d930416e7c9c42",
+};
+
+const firebase = initializeApp(firebaseConfig);
 
 const socketio = new VueSocketIO({
   debug: false,
